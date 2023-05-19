f617161625479540beacf7218752f370c8fe4e63
shwalee90 <qnfrgmfr16@gmail.com>
2023-05-19 09:03:28 +0900

    context finish

---
+++ ch04/ch04_6/src/utils/imageFileReaderP.ts
@@ -0,0 +1,13 @@
+//import { rejects } from "assert";
+
+export const imageFileReaderP = (file: Blob) =>
+  new Promise<string>((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.onload = (e: ProgressEvent<FileReader>) => {
+      const result = e.target?.result;
+
+      if (result && typeof result === "string") resolve(result);
+      else reject(new Error(`imageFileReaderP : can't read image file`));
+    };
+    fileReader.readAsDataURL(file);
+  });
