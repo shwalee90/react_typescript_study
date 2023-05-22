94a3bf3e3008811f24abff866921ef688c8f1139
shwalee90 <qnfrgmfr16@gmail.com>
2023-05-22 09:33:34 +0900

    add card

--- ch05/ch05_2/src/store/cards/types.ts
+++ ch05/ch05_2/src/store/cards/types.ts
@@ -1,3 +1,14 @@
 import type { Action } from "redux";
-export type State = any
-export type Actions = Action
+import * as D from "../../data";
+
+export type Card = D.ICard;
+export type State = Card[];
+
+export type AddCardAction = Action<"@cards/addCard"> & {
+  payload: Card;
+};
+export type RemoveCardAction = Action<"@cards/removeCard"> & {
+  payload: string;
+};
+
+export type Actions = AddCardAction | RemoveCardAction;
