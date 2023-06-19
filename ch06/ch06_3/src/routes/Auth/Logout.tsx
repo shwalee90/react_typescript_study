2cb0e3459d25ade0afe7f6194d7d1458466f1990
shwalee90 <qnfrgmfr16@gmail.com>
2023-06-19 15:42:14 +0900

    logout 추가

--- ch06/ch06_3/src/routes/Auth/Logout.tsx
+++ ch06/ch06_3/src/routes/Auth/Logout.tsx
@@ -1,8 +1,45 @@
-export default function CopyMe() {
+import { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+import { Modal, ModalContent, ModalAction } from "../../theme/daisyui";
+import { useToggle } from "../../hooks";
+import { useAuth } from "../../contexts";
+
+export default function Logout() {
+  const [open, toggleOpen] = useToggle(true);
+
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+  const onAccept = useCallback(() => {
+    logout(() => {
+      toggleOpen();
+      navigate("/");
+    });
+  }, [navigate, toggleOpen, logout]);
+
+  const onCancel = useCallback(() => {
+    toggleOpen();
+    navigate(-1);
+  }, [navigate, toggleOpen]);
+
   return (
-    <section className="mt-4">
-      <h2 className="font-bold text-5xl text-center">CopyMe</h2>
-      <div className="mt-4"></div>
-    </section>
+    <Modal open={open}>
+      <ModalContent
+        closeIconClassName="btn-primary btn-outline"
+        onCloseIconClicked={onCancel}
+      >
+        <p className="text-xl text-center">
+          {" "}
+          Are you sure you want to logout ?
+        </p>
+        <ModalAction>
+          <button className="btn btn-primary btn-sm" onClick={onAccept}>
+            Logout
+          </button>
+          <button className="btn btn-secondary btn-sm" onClick={onCancel}>
+            Cancel
+          </button>
+        </ModalAction>
+      </ModalContent>
+    </Modal>
   );
 }
--- ch06/ch06_3/src/routes/Auth/RequireAuth.tsx
+++ ch06/ch06_3/src/routes/Auth/RequireAuth.tsx
@@ -1,8 +1,19 @@
-export default function CopyMe() {
-  return (
-    <section className="mt-4">
-      <h2 className="font-bold text-5xl text-center">CopyMe</h2>
-      <div className="mt-4"></div>
-    </section>
-  );
-}
+import type { FC, PropsWithChildren } from "react";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts";
+
+type RequireAuthProps = {};
+
+const RequireAuth: FC<PropsWithChildren<RequireAuthProps>> = ({ children }) => {
+  const { loggedUser } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!loggedUser) navigate(-1); //허가 되지 않은 사용자 :이전페이지로 돌아감
+  }, [loggedUser, navigate]);
+
+  return <> {children}</>;
+};
+
+export default RequireAuth;
